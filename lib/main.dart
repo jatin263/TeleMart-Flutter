524228aa72fbade09c5f6c7aa40994eb0b1b5d0d
Jatin Kumawat <jatinkumawat20@gmail.com>
2023-04-15 03:11:17 +0530

    login design

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,16 +22,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -72,46 +53,89 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Color.fromRGBO(78, 168, 241, 1),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
           children: [
             Padding(
-                padding: EdgeInsets.only(top: 80, bottom: 50),
+                padding: EdgeInsets.only(top: 150, bottom: 50),
                 child: Text(
                   "Welcome to TeleMart",
-                  style: TextStyle(fontSize: 25),
+                  style: TextStyle(fontSize: 35),
                   textAlign: TextAlign.center,
                 )),
             Container(
               padding: EdgeInsets.only(bottom: 50),
-              width: 250,
+              width: 200,
               child: Column(children: [
                 Padding(
-                  padding: EdgeInsets.only(bottom: 50),
+                  padding: EdgeInsets.only(bottom: 25),
                   child: TextField(
                     controller: nameController,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              width: 3,
+                              color: Color.fromRGBO(247, 244, 244, 1))),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(width: 3, color: Colors.black)),
                       labelText: 'Username',
                     ),
-                    style: TextStyle(height: 1.5),
+                    style: TextStyle(
+                        height: 1.5,
+                        color: Color.fromARGB(255, 245, 246, 245),
+                        fontSize: 25),
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(bottom: 50),
+                  padding: EdgeInsets.only(bottom: 55),
                   child: TextField(
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
                     controller: nameController,
                     decoration: InputDecoration(
-                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              width: 3,
+                              color: Color.fromRGBO(255, 255, 255, 1.0))),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(width: 3, color: Colors.black)),
                       labelText: 'Password',
                     ),
-                    style: TextStyle(height: 1.5),
+                    style: TextStyle(
+                        height: 1.5,
+                        color: Color.fromARGB(255, 245, 246, 245),
+                        fontSize: 25),
                   ),
-                )
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  // style: ButtonStyle(elevation: MaterialStateProperty(12.0 )),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 226, 113, 27),
+                      elevation: 15.0,
+                      textStyle:
+                          const TextStyle(color: Colors.black, fontSize: 20)),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
               ]),
-            )
+            ),
+            Flexible(
+                child: Text(
+              "A product by JK",
+              style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+            ))
           ],
         ),
       ),
